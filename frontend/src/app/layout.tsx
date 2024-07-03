323d14f9bc331c77db538653501d14bd147405ab
douglasdans <douglas.dans@gmail.com>
2024-07-03 15:32:17 -0300

    style(index): adiciona anti-serrilhado na fonte

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={openSans.className}>{children}</body>
+      <body className={openSans.className + " antialiased"}>{children}</body>
     </html>
   )
 }
