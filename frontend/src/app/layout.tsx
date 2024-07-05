0d9233aac6173f0b2b0dbf5444cc628d60852cae
douglasdans <douglas.dans@gmail.com>
2024-07-05 18:37:56 -0300

    refactor(layout.tsx): remove variáveis de fonte não utilizadas

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from 'next'
-import { Open_Sans, Poppins } from 'next/font/google'
 import '@/styles/global.scss'
-
-const openSans = Open_Sans({
-  subsets: ['latin'],
-  display: 'swap'
-})
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ["600"],
-  variable: "--font-poppins"
-})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
