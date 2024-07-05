b2c13169d7b3cb6c92e3d08dc6b7462ea060fb31
douglasdans <douglas.dans@gmail.com>
2024-07-05 18:34:00 -0300

    style(fonts): adiciona fonte Poppins aos títulos

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+import { Open_Sans, Poppins } from 'next/font/google'
 import '@/styles/global.scss'
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  display: 'swap'
+})
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ["600"],
+  variable: "--font-poppins"
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={openSans.className + ' antialiased'}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
