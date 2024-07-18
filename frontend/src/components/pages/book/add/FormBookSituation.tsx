13e246c0820b5c2745c2a6929ef48143f46af8ca
douglasdans <douglas.dans@gmail.com>
2024-07-17 21:37:22 -0300

    feat(frontend | BookAdd): adiciona itens de situação do livro

--- frontend/src/components/pages/book/add/FormBookSituation.tsx
+++ frontend/src/components/pages/book/add/FormBookSituation.tsx
@@ -1,5 +1,51 @@
+'use client'
+
+import BookStatusButton from "@/components/ui/buttons/BookStatusButton";
+import PrimaryButton from "@/components/ui/buttons/PrimaryButton";
+import styles from '@/styles/pages/book/add/form-book-situation.module.scss'
+import { usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { ReactEventHandler, useState } from "react";
+
 export default function FormBookSituation() {
+  // const [bookStatus, setBookStatus] = useState([])
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  function setBookStatusState(e: Event, param: string) {
+    e.preventDefault()
+
+    const params = new URLSearchParams(searchParams)
+    params.set("bookStatus", param)
+
+    replace(`${pathname}?${params.toString()}`, { scroll: false })
+  }
+
   return (
-    <div>FormBookSituation</div>
+    <div className={styles.container}>
+      <div className={styles.itemForm}>
+        <label>Status do Livro</label>
+        <div className={styles.bookStatusWrapper}>
+          <BookStatusButton onClick={setBookStatusState} type="comprar" value={"comprar"} />
+          <BookStatusButton onClick={setBookStatusState} type="naoIniciado" value={"naoIniciado"} />
+          <BookStatusButton onClick={setBookStatusState} type="andamento" value={"andamento"} />
+          <BookStatusButton onClick={setBookStatusState} type="concluido" value={"concluido"} />
+          <BookStatusButton onClick={setBookStatusState} type="desistiu" value={"desistiu"} />
+        </div>
+      </div>
+
+      <div className={styles.itemForm}>
+        <label>Adicionar à Coleção</label>
+        <input type="text" disabled />
+      </div>
+
+      <div className={styles.itemForm}>
+        <label>Finalizar Cadastro</label>
+        <PrimaryButton>
+          Cadastrar Livro
+        </PrimaryButton>
+      </div>
+    </div>
   )
 }
---
+++ frontend/src/components/ui/buttons/BookStatusButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from '@/styles/ui/buttons/book-status-button.module.scss'
+
+type Props = {
+  type: "comprar" | "naoIniciado" | "andamento" | "concluido" | "desistiu",
+  onClick: Function,
+  value: string
+}
+
+export default function BookStatusButton({ type, onClick, value }: Props) {
+
+  const tagTypes = {
+    comprar: {
+      value: "Comprar",
+      style: styles.comprar
+    },
+    naoIniciado: {
+      value: "Não Iniciado",
+      style: styles.nIniciado
+    },
+    andamento: {
+      value: "Em Andamento",
+      style: styles.andamento
+    },
+    concluido: {
+      value: "Concluído",
+      style: styles.concluido
+    },
+    desistiu: {
+      value: "Desistiu",
+      style: styles.desistiu
+    }
+  }
+
+  return (
+    <button onClick={(e) => { onClick(e, value) }} className={styles.button + " " + tagTypes[type].style}>
+      {tagTypes[type].value}
+    </button>
+  )
+}
