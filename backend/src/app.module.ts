c9fcc73a1b56113815df0c5fe9b0a72b1fda033b
douglasdans <douglas.dans@gmail.com>
2024-07-22 18:22:21 -0300

    feat(backend | nestjs): adiciona arquivos de manipulação de dados crud

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { AuthorModule } from './author/author.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthorModule],
 })
 export class AppModule {}
