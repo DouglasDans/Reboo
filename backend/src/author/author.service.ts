a2ce772315fae595e51a4b6819ab1e2b092e5e56
douglasdans <douglas.dans@gmail.com>
2024-07-22 22:01:32 -0300

    feat(backend | author): adiciona lógica crud para entidade author

--- backend/src/author/author.service.ts
+++ backend/src/author/author.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { CreateAuthorDto } from './dto/create-author.dto'
 import { UpdateAuthorDto } from './dto/update-author.dto'
+import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
 export class AuthorService {
+  constructor(private prisma: PrismaService) {}
+
   create(createAuthorDto: CreateAuthorDto) {
-    return 'This action adds a new author'
+    return this.prisma.author.create({
+      data: createAuthorDto,
+    })
   }
 
-  findAll() {
-    return `This action returns all author`
+  async findAll() {
+    return await this.prisma.author.findMany()
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} author`
+    return this.prisma.author
+      .findUniqueOrThrow({
+        where: { id },
+      })
+      .catch((e) => {
+        throw new HttpException(
+          {
+            message: 'Dados não foram encontrados no banco - ' + e,
+            statusCode: HttpStatus.NOT_FOUND,
+          },
+          HttpStatus.NOT_FOUND,
+        )
+      })
   }
 
   update(id: number, updateAuthorDto: UpdateAuthorDto) {
-    return `This action updates a #${id} author`
+    return this.prisma.author
+      .update({
+        where: { id },
+        data: updateAuthorDto,
+      })
+      .catch((e) => {
+        throw new HttpException(
+          {
+            message: 'Dados não foram encontrados no banco - ' + e.meta.cause,
+            statusCode: HttpStatus.NOT_FOUND,
+          },
+          HttpStatus.NOT_FOUND,
+        )
+      })
   }
 
   remove(id: number) {
-    return `This action removes a #${id} author`
+    return this.prisma.author
+      .delete({
+        where: { id },
+      })
+      .catch((e) => {
+        throw new HttpException(
+          {
+            message: 'Dados não foram encontrados no banco - ' + e.meta.cause,
+            statusCode: HttpStatus.NOT_FOUND,
+          },
+          HttpStatus.NOT_FOUND,
+        )
+      })
   }
 }
--- backend/src/author/dto/create-author.dto.ts
+++ backend/src/author/dto/create-author.dto.ts
@@ -1,1 +1,6 @@
-export class CreateAuthorDto {}
+import { IsString } from 'class-validator'
+
+export class CreateAuthorDto {
+  @IsString()
+  name: string
+}
