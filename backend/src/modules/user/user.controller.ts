f144cc0d161db05481144c68a514e90f711e03e0
Douglas Nascimento <56117204+DouglasDans@users.noreply.github.com>
2024-08-10 18:49:52 +0000

    fix(backend | user): corrige endpoint do usuário

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -11,7 +11,7 @@ import { UserService } from './user.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 
-@Controller('api/v1/[userId]')
+@Controller('api/v1/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
