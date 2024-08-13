a96e00cde3650f7f7fae78751b5df0ab55b40ba1
douglasdans <douglas.dans@gmail.com>
2024-08-13 19:35:55 -0300

    feat(backend | data-services): adiciona módulos de acesso ao Prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,7 @@
 import { Module } from '@nestjs/common'
-import { AuthorModule } from './modules/author/author.module'
-import { PublisherModule } from './modules/publisher/publisher.module'
-import { CategoryModule } from './modules/category/category.module'
-import { CollectionModule } from './modules/collection/collection.module'
-import { UserModule } from './modules/user/user.module'
-import { BookModule } from './modules/book/book.module'
-import { ReadingSessionModule } from './modules/reading-session/reading-session.module'
-import { BookCollectionModule } from './modules/book-collection/book-collection.module'
+import { DataServicesModule } from './services/data-services/data-services.module'
 
 @Module({
-  imports: [
-    AuthorModule,
-    PublisherModule,
-    CategoryModule,
-    CollectionModule,
-    UserModule,
-    BookModule,
-    ReadingSessionModule,
-    BookCollectionModule,
-  ],
+  imports: [DataServicesModule],
 })
 export class AppModule {}
