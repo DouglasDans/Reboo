b85ba32e6eae5db4c37f80b61081f6f9d17de12f
douglasdans <douglas.dans@gmail.com>
2024-08-14 17:56:56 -0300

    feat(backend | app.module): adiciona use-cases no app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common'
 import { DataServicesModule } from './services/data-services/data-services.module'
 import { AuthorUseCaseModule } from './use-cases/author'
+import { BookUseCaseModule } from './use-cases/book'
+import { CategoryUseCaseModule } from './use-cases/category'
+import { CollectionUseCaseModule } from './use-cases/collection'
+import { PublisherUseCaseModule } from './use-cases/publisher'
+import { ReadingSessionUseCaseModule } from './use-cases/reading-session'
+import { UserUseCaseModule } from './use-cases/user'
 
 @Module({
-  imports: [DataServicesModule, AuthorUseCaseModule],
+  imports: [
+    DataServicesModule,
+    AuthorUseCaseModule,
+    BookUseCaseModule,
+    CategoryUseCaseModule,
+    CollectionUseCaseModule,
+    PublisherUseCaseModule,
+    ReadingSessionUseCaseModule,
+    UserUseCaseModule,
+  ],
 })
 export class AppModule {}
