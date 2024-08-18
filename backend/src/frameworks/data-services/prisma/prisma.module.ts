9cc6ad105ef9d46260c1b72e3fbb3d6ee1f476cb
douglasdans <douglas.dans@gmail.com>
2024-08-18 19:46:19 -0300

    refactor(backend | controller): altera caminho do PrismaClient

--- backend/src/frameworks/data-services/prisma/prisma.module.ts
+++ backend/src/frameworks/data-services/prisma/prisma.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma.service'
 
 @Module({
-  providers: [PrismaService],
   exports: [PrismaService],
+  providers: [PrismaService],
 })
 export class PrismaModule {}
