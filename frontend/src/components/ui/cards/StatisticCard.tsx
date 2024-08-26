e5d61be1887f92db35933f87bfb6b194c58433fd
douglasdans <douglas.dans@gmail.com>
2024-08-25 22:25:36 -0300

    feat(frontend | statisticCard): altera parte da estrutura para receber parâmetros

--- frontend/src/components/ui/cards/StatisticCard.tsx
+++ frontend/src/components/ui/cards/StatisticCard.tsx
@@ -3,13 +3,29 @@ import styles from './statistic-card.module.scss'
 
 type Props = {}
 
+const data = {
+  title: "Páginas Lidas Hoje",
+  mainNumber: 56,
+  mainSubtitle: "Páginas",
+  goals: {
+    type: "diárias",
+    value: 10
+  },
+  trendingGoals: 89
+}
+
 export default function StatisticCard({ }: Props) {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.content}>
-        <span className={styles.cardTitle}>Páginas lidas Hoje</span>
-        <span className={styles.statNumber}>56</span>
-        <small>Meta diária: 10</small>
+        <span className={styles.cardTitle}>{data.title}</span>
+        <div className={styles.statNumber}>
+          <span className={styles.mainNumber}>{data.mainNumber}</span>
+          <span className={styles.mainSubtitle}> {data.mainSubtitle}</span>
+        </div>
+        <div className={styles.goals}>
+          <small>Meta diária: 10</small>
+        </div>
       </div>
       <div className={styles.icon}><Book /></div>
     </div>
