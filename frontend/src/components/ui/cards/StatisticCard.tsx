4e5c3acd7656a2fb8a0945b1e3fd2808a37f9b68
douglasdans <douglas.dans@gmail.com>
2024-08-27 08:58:49 -0300

    feat(frontend | dashboard): altera tipos do statisticCard

--- frontend/src/components/ui/cards/StatisticCard.tsx
+++ frontend/src/components/ui/cards/StatisticCard.tsx
@@ -1,9 +1,23 @@
 import { Book, TrendingUpRounded } from '@mui/icons-material'
 import styles from './statistic-card.module.scss'
+import { ReactNode } from 'react'
 
-type Props = {}
+type Props = {
+  title: string
+  mainNumber: number
+  mainSubtitle: string
+  cardIcon: ReactNode
+  goals: {
+    type: string
+    value: number
+  }
+  trends?: {
+    trendUp: boolean
+    trendValue: number
+  }
+}
 
-const data = {
+const obj = {
   title: "Páginas Lidas Hoje",
   mainNumber: 56,
   mainSubtitle: "Páginas",
@@ -11,33 +25,40 @@ const data = {
   goals: {
     type: "Meta diária:",
     value: 10,
+  },
+  trends: {
     trendUp: true,
     trendValue: 189
-  },
+  }
 }
 
-export default function StatisticCard({ }: Props) {
+export default function StatisticCard(data: Props) {
+  data = obj
   return (
     <div className={styles.cardContainer}>
       <div className={styles.content}>
         <span className={styles.cardTitle}>{data.title}</span>
+
         <div className={styles.statNumber}>
           <span className={styles.mainNumber}>{data.mainNumber}</span>
           <span className={styles.mainSubtitle}> {data.mainSubtitle}</span>
         </div>
+
         <div className={styles.goals}>
-          <div>
-            <div
-              className=
-              {styles.goalIcon + " " +
-                (data.goals.trendUp ? styles.goalTrendUp : styles.goalTrendDown)}
-            >
-              <TrendingUpRounded />
+          {data.trends ?
+            <div>
+              <div
+                className={styles.goalIcon + " " +
+                  (data.trends.trendUp ? styles.goalTrendUp : styles.goalTrendDown)}
+              >
+                <TrendingUpRounded />
+              </div>
+              <small className={styles.goalText + " " + (data.trends.trendUp ? styles.goalTrendUp : styles.goalTrendDown)}>{data.trends.trendValue}%</small>
             </div>
-            <small className={styles.goalText + " " + (data.goals.trendUp ? styles.goalTrendUp : styles.goalTrendDown)}>{data.goals.trendValue}%</small>
-          </div>
+            : ""}
           <small>{data.goals.type} {data.goals.value}</small>
         </div>
+
       </div>
       <div className={styles.icon}>{data.cardIcon}</div>
     </div>
