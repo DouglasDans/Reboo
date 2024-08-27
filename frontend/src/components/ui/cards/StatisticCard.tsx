3260172279af24b1ac3ff94ce13e9dd634797d58
douglasdans <douglas.dans@gmail.com>
2024-08-26 22:06:02 -0300

    feat(frontend | dashboard): atualiza estrutura do statisticCard

--- frontend/src/components/ui/cards/StatisticCard.tsx
+++ frontend/src/components/ui/cards/StatisticCard.tsx
@@ -1,4 +1,4 @@
-import { Book } from '@mui/icons-material'
+import { Book, TrendingUpRounded } from '@mui/icons-material'
 import styles from './statistic-card.module.scss'
 
 type Props = {}
@@ -7,11 +7,13 @@ const data = {
   title: "Páginas Lidas Hoje",
   mainNumber: 56,
   mainSubtitle: "Páginas",
+  cardIcon: <Book />,
   goals: {
-    type: "diárias",
-    value: 10
+    type: "Meta diária:",
+    value: 10,
+    trendUp: true,
+    trendValue: 189
   },
-  trendingGoals: 89
 }
 
 export default function StatisticCard({ }: Props) {
@@ -24,10 +26,20 @@ export default function StatisticCard({ }: Props) {
           <span className={styles.mainSubtitle}> {data.mainSubtitle}</span>
         </div>
         <div className={styles.goals}>
-          <small>Meta diária: 10</small>
+          <div>
+            <div
+              className=
+              {styles.goalIcon + " " +
+                (data.goals.trendUp ? styles.goalTrendUp : styles.goalTrendDown)}
+            >
+              <TrendingUpRounded />
+            </div>
+            <small className={styles.goalText + " " + (data.goals.trendUp ? styles.goalTrendUp : styles.goalTrendDown)}>{data.goals.trendValue}%</small>
+          </div>
+          <small>{data.goals.type} {data.goals.value}</small>
         </div>
       </div>
-      <div className={styles.icon}><Book /></div>
+      <div className={styles.icon}>{data.cardIcon}</div>
     </div>
   )
 }
