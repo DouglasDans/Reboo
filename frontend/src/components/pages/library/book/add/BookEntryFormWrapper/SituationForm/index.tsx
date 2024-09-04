80a8186769e181f413ef940e34f8e96da46b8969
douglasdans <douglas.dans@gmail.com>
2024-09-04 15:42:00 -0300

    refactor(frontend | BookEntryForm): refatora toda a estrutura dos componentes de formulário da página book/add

---
+++ frontend/src/components/pages/library/book/add/BookEntryFormWrapper/SituationForm/index.tsx
@@ -0,0 +1,50 @@
+import StatusRadioButton from '@/components/ui/forms/StatusRadioButton'
+import styles from './index.module.scss'
+import Button from '@/components/ui/buttons/button'
+
+export default function SituationForm() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.itemForm}>
+
+        <label>Status do Livro</label>
+        <div className={styles.bookStatusWrapper}>
+          <StatusRadioButton
+            type="BUY"
+            id="status1"
+            name="bookStatus"
+            defaultChecked
+          />
+          <StatusRadioButton
+            type="NOT_STARTED"
+            id="status2"
+            name="bookStatus"
+          />
+          <StatusRadioButton
+            type="IN_PROGRESS"
+            id="status3"
+            name="bookStatus"
+          />
+          <StatusRadioButton type="COMPLETED" id="status4" name="bookStatus" />
+          <StatusRadioButton type="GIVEN_UP" id="status5" name="bookStatus" />
+        </div>
+
+      </div>
+
+      <div className={styles.itemForm}>
+        <label>Quantidade de Páginas Lidas</label>
+        <input name={"bookReadPages"} type="number" defaultValue={0} required />
+      </div>
+
+      {/* <div className={styles.itemForm}>
+        <label>Adicionar à Coleção</label>
+        <input type="text" disabled />
+      </div> */}
+
+      <div className={styles.itemForm}>
+        <label>Finalizar Cadastro</label>
+        <Button>Cadastrar Livro</Button>
+      </div>
+    </div>
+  )
+}
--- frontend/src/components/pages/library/book/add/BookEntryFormWrapper/index.tsx
+++ frontend/src/components/pages/library/book/add/BookEntryFormWrapper/index.tsx
@@ -1,12 +1,14 @@
 import postBook from '@/actions/book/postBook'
 import styles from './index.module.scss'
 import DetailsForm from './DetailsForm'
+import SituationForm from './SituationForm'
 
 export default function BookEntryFormWrapper() {
   return (
     <section >
       <form action={postBook} className={styles.form}>
         <DetailsForm />
+        <SituationForm />
       </form>
     </section>
   )
