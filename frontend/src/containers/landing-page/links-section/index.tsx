853b5fcafb3cffe4922d42321434058011a5030d
douglasdans <douglas.dans@gmail.com>
2024-09-29 14:05:09 -0300

    feat(frontend | landingPage): adiciona linha de texto na section de links

--- frontend/src/containers/landing-page/links-section/index.tsx
+++ frontend/src/containers/landing-page/links-section/index.tsx
@@ -3,13 +3,11 @@ import styles from './index.module.scss'
 import Button from '@/components/buttons/button'
 import Link from 'next/link'
 
-type Props = {}
-
-export default function LandingLinksContainer({ }: Props) {
+export default function LandingLinksContainer() {
   return (
     <section className={styles.container}>
       <Image src={"/reboo-logo-text.svg"} height={82} width={239} alt='Logo do aplicativo. 2 Livros encostados com o nome "Reboo"' />
-
+      <span>Acesse, crie sua conta e comece a acompanhar seus hábitos de leitura!</span>
       <div>
         <Link href={"/login"}>
           <Button>Começar Agora</Button>
