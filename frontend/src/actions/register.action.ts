34b5bc15af7bb2029d9f97eac67401e477c51a5b
douglasdans <douglas.dans@gmail.com>
2024-10-02 10:30:39 -0300

    refactor(frontend | register): remove validação de senha da action

--- frontend/src/actions/register.action.ts
+++ frontend/src/actions/register.action.ts
@@ -1,15 +1,3 @@
 "use server"
 
-export default async function makeRegister(formData: FormData) {
-  const password = formData.get("password") as string
-  const confirmPassword = formData.get("confirmPassword") as string
-
-  if (!verifyPasswordMatch(password, confirmPassword)) {
-    throw new Error("Passwords do not match")
-  }
-  console.log(password, confirmPassword)
-}
-
-function verifyPasswordMatch(password: string, confirmPassword: string): boolean {
-  return password === confirmPassword
-}
+export default async function makeRegister(formData: FormData) {}
