8af948ca09bfff99c6b16912cd883d5a9584aaba
douglasdans <douglas.dans@gmail.com>
2024-10-10 21:26:17 -0300

    feat(frontend | user-options-menu): atualiza estilo e acesso à informações no componente

--- frontend/src/components/dropdown-menu/menus/UserOptionsMenu/index.tsx
+++ frontend/src/components/dropdown-menu/menus/UserOptionsMenu/index.tsx
@@ -2,20 +2,25 @@ import Icon from '@/components/icon'
 import styles from './index.module.scss'
 import Button from '@/components/buttons/button'
 import Link from 'next/link'
+import { User } from '@/services/rebooAPI/api.types'
 
-export default function UserOptionsMenu() {
+type Props = {
+  user: User
+}
+
+export default function UserOptionsMenu({ user }: Props) {
   return (
     <div className={styles.container}>
       <div className={styles.user}>
         <Icon name="person" />
         <div className={styles.data}>
-          <span>User</span>
-          <small>userEmail</small>
+          <span>{user.name}</span>
+          <small>{user.email}</small>
         </div>
       </div>
       <div className={styles.options}>
-        <Link href={"/logout"}>
-          <Button startDecorator={<Icon name='logout' />}>Fazer Logout</Button>
+        <Link className={styles.optionItem} href={"/logout"}>
+          <Button fullWidth variant='secondary' startDecorator={<Icon name='logout' />}>Fazer Logout</Button>
         </Link>
       </div>
     </div>
