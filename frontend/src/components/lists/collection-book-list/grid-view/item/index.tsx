3a14162249268034acda39533006e0b44970f4c7
douglasdans <douglas.dans@gmail.com>
2024-10-16 23:52:23 -0300

    fix(frontend | book-list): corrige alinhamento da lista de itens

--- frontend/src/components/lists/collection-book-list/grid-view/item/index.tsx
+++ frontend/src/components/lists/collection-book-list/grid-view/item/index.tsx
@@ -1,16 +1,43 @@
+'use client'
+
 import Link from 'next/link'
 import styles from './index.module.scss'
 import { Book } from '@/services/rebooAPI/api.types'
+import { useEffect, useState } from 'react'
 
 type Props = {
   book: Book
 }
 
+export function isValidImageUrl(url: string): Promise<boolean> {
+  return new Promise(resolve => {
+    const img = new Image()
+    img.onload = () => resolve(true)
+    img.onerror = () => resolve(false)
+    img.src = url
+  })
+}
+
 export default function GridBookItem({ book }: Props) {
+  const [coverImage, setCoverImage] = useState<string | null>(null)
+
+  useEffect(() => {
+    setCoverImage(null)
+    isValidImageUrl(book.coverImage).then((res) => {
+      if (res) {
+        setCoverImage(book.coverImage)
+      }
+    })
+  }, [book.coverImage])
+
   return (
     <Link href={`./library/book/${book.id}`} className={styles.content}>
       <div className={styles.bookCoverImage}>
-        <img className={styles.coverImage} src={book.coverImage ? book.coverImage : "/book-image-placeholder.png"} alt="" />
+        <img
+          src={coverImage || "/book-image-placeholder.png"}
+          className={styles.coverImage}
+          alt=""
+        />
       </div>
       <div className={styles.bookCoverInfo}>
         <small className={styles.authorName}>{book.authors?.length ? book.authors[0].author?.name : ""}</small>
