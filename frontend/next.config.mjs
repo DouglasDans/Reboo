f07a8a48cca5beb79083080eb19ce264265eb53f
Douglas Nascimento <56117204+DouglasDans@users.noreply.github.com>
2024-10-18 19:06:13 +0000

    chore(frontend | next.config.mjs): adiciona configuração experimental para serverActions

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", process.env.NEXT_ORIGIN_SERVER_ACTIONS],
+    },
+  },
 }
 
 export default nextConfig
